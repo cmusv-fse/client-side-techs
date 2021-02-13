0733ed1b3a4c15418790bc2169baa6696d18425f
Hans Li <lizihan0328@gmail.com>
2021-02-12 17:18:24 -0800

    Format and upgrade dependencies for webpack demo

--- Webpack/src/index.js
+++ Webpack/src/index.js
@@ -1,15 +1,20 @@
-import './index.less';
-import Navigo from 'navigo';
-import Home from './view/home.html';
-import Error from './view/error.html';
+import "./index.less";
+import Navigo from "navigo";
+import Home from "./view/home.html";
+import ErrorPage from "./view/error.html";
+import AnotherPage from "./view/another-page.html";
 
-const app = document.getElementById('app');
-const router = new Navigo(null, true, '#');
+const app = document.getElementById("app");
+const router = new Navigo("/");
 
-router.on('/', function () {
-    app.innerHTML = Home
-}).resolve();
-
-router.notFound(function () {
-    app.innerHTML = Error
-}).resolve();
+router
+  .on("/", () => {
+    app.innerHTML = Home;
+  })
+  .on("/another-page", () => {
+    app.innerHTML = AnotherPage;
+  })
+  .notFound(() => {
+    app.innerHTML = ErrorPage;
+  })
+  .resolve();
--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -1,66 +1,57 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HappyPack = require('happypack');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'production',
-    entry: {
-        app: './src/index.js',
-    },
-    devtool: 'inline-source-map',
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'ESN',
-            template: 'index.html'
-        }),
-        new HappyPack({
-            cache: true,
-            loaders: [{
-                loader: 'babel-loader'
-            }]
-        })
+  mode: "production",
+  entry: {
+    app: "./src/index.js",
+  },
+  devtool: "inline-source-map",
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "ESN",
+      template: "index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(less)$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "less-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.(js)$/,
-                loaders: ['happypack/loader']
-            },
-            {
-                test: /\.html$/,
-                loader: 'html-loader',
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(less)$/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    }, {
-                        loader: 'css-loader'
-                    }, {
-                        loader: 'less-loader', options: {
-                            sourceMap: true
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            }
-        ]
-    }
+  },
 };
